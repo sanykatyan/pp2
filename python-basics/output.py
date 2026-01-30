6d261040ba8701e0e88d704845f0fe6ce986a82b
ㅤкишки <heteroky@yandex.kz>
2026-01-30 15:00:42 +0500

    add practice1 python basics examples

--- python-basics/output.py
+++ python-basics/output.py
@@ -11,3 +11,13 @@ print("'This will cause an error'")
 
 print("Hello World!", end=" ")
 print("I will print on the same line.")
+
+
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
