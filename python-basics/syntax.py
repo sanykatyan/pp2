ae7c4d3046b64960b95e282f15daa4bf9ed61b1d
ㅤкишки <heteroky@yandex.kz>
2026-01-30 14:56:00 +0500

    add practice1 python basics examples

---
+++ python-basics/syntax.py
@@ -0,0 +1,25 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+
+if 10 < 2*2*2*2 :
+    print("yes")
+
+
+
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+
+
+#This is a comment.
+print("Hello, World!")
+
+
+z = 4342525242
+print(z)
+
+
