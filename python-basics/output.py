e355a55fe7355d040229ad5376e772c78e69ddbf
ㅤкишки <heteroky@yandex.kz>
2026-01-30 14:59:42 +0500

    add practice1 python basics examples

---
+++ python-basics/output.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+print("This will work!")
+print('This will also work!')
+
+print("'This will cause an error'")
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
