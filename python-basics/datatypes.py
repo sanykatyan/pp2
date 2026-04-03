7ad421e1712a8d2993efada9107750d613070ce8
ㅤкишки <heteroky@yandex.kz>
2026-04-03 12:39:04 +0500

    remove old folders and README

--- python-basics/datatypes.py
+++
@@ -1,7 +0,0 @@
-x = str("Hello World")
-x = int(20)	
-x = float(20.5)	
-x = complex(1j)	
-x = list(("apple", "banana", "cherry"))	
-x = tuple(("apple", "banana", "cherry"))	
-
--- python-basics/home.py
+++
@@ -1,5 +0,0 @@
-print("Hello, World!")
-print("привет")
-print("хай ")
-print("ура")
-print("пока")
